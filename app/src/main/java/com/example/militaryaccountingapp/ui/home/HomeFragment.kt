7e97733ce596fa05f2c2c04efe5c9089eea97133
Erik <Gitarist.Erik@gmail.com>
2023-02-23 00:31:18 +0200

    Add search view and update tabs

--- app/src/main/java/com/example/militaryaccountingapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/militaryaccountingapp/ui/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.example.militaryaccountingapp.ui.home
 
-import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.militaryaccountingapp.R
 import com.example.militaryaccountingapp.databinding.FragmentHomeBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import android.R as AndroidR
 
 class HomeFragment : Fragment() {
 
@@ -26,32 +26,22 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         binding.apply {
-            viewPager.adapter = HomeViewPagerAdapter(this@HomeFragment.requireActivity())
+            val tabTitles = resources.getStringArray(R.array.home_tab_title)
 
-            TabLayoutMediator(
-                tabLayout,
-                viewPager
-            ) { tab, position ->
-                when (position) {
-                    0 -> tab.text = "Categories"
-                    1 -> tab.text = "Items"
-                }
+            viewPager.adapter = HomeViewPagerAdapter(
+                tabTitles.size, childFragmentManager, lifecycle
+            )
+
+            viewPager.offscreenPageLimit = tabTitles.size
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = tabTitles[position]
             }.attach()
         }
 
-
-
-//        binding.searchView.setupWithSearchBar(binding.searchBar)
-        /*val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }*/
         return binding.root
     }
 
@@ -67,7 +57,7 @@ class TempAdapter(private val items: Array<String>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val listItem: View = LayoutInflater.from(parent.context).inflate(
-            R.layout.simple_list_item_2, parent, false
+            AndroidR.layout.simple_list_item_2, parent, false
         )
         return ViewHolder(listItem)
     }
@@ -83,36 +73,7 @@ class TempAdapter(private val items: Array<String>) :
     companion object {
         class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             fun bind(item: String) {
-                itemView.findViewById<TextView>(R.id.text1).text = item
-            }
-        }
-    }
-}
-
-// adapter for horizontal recycler view in home fragment like a card view with image and text without using card view
-class HorizontalAdapter(private val items: Array<String>) :
-    RecyclerView.Adapter<HorizontalAdapter.Companion.ViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val listItem: View = LayoutInflater.from(parent.context).inflate(
-            R.layout.simple_list_item_2, parent, false
-        )
-        return ViewHolder(listItem)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(items[position])
-    }
-
-    override fun getItemCount(): Int {
-        return items.size
-    }
-
-    companion object {
-        class ViewHolder(val itemView: View) : RecyclerView.ViewHolder(itemView) {
-            fun bind(item: String) {
-                itemView.findViewById<TextView>(R.id.text1).text = item
-                itemView.findViewById<TextView>(R.id.text2).text = item
+                itemView.findViewById<TextView>(AndroidR.id.text1).text = item
             }
         }
     }
--- app/src/main/java/com/example/militaryaccountingapp/ui/home/HomeViewPagerAdapter.kt
+++ app/src/main/java/com/example/militaryaccountingapp/ui/home/HomeViewPagerAdapter.kt
@@ -1,12 +1,16 @@
 package com.example.militaryaccountingapp.ui.home
 
-import android.content.Context
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class HomeViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity)  {
-    override fun getItemCount(): Int = 2
+class HomeViewPagerAdapter(
+    private val tabSize: Int,
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle,
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int = tabSize
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
