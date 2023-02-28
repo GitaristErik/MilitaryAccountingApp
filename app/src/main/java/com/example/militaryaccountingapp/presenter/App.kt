8fef1bd6608a3c1fdf9c1c0a761e53c0243bab59
Erik <Gitarist.Erik@gmail.com>
2023-02-28 19:55:16 +0200

    Timber connected

--- app/src/main/java/com/example/militaryaccountingapp/presenter/App.kt
+++ app/src/main/java/com/example/militaryaccountingapp/presenter/App.kt
@@ -1,12 +1,17 @@
 package com.example.militaryaccountingapp.presenter
 
 import android.app.Application
+import com.example.militaryaccountingapp.BuildConfig
 import com.google.android.material.color.DynamicColors
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 open class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        if (BuildConfig.DEBUG) Timber.plant(DebugTree())
+        Timber.d("onCreate")
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
 }
