681964f4c9e91d360cf426bff1b9450ae5c9e324
Erik <Gitarist.Erik@gmail.com>
2023-05-07 21:06:34 +0300

    Connect filters to StatisticsFragment

--- app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/statistics/StatisticsFragment.kt
@@ -3,23 +3,62 @@ package com.example.militaryaccountingapp.presenter.fragment.statistics
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.example.militaryaccountingapp.R
 import com.example.militaryaccountingapp.databinding.FragmentStatisticsBinding
 import com.example.militaryaccountingapp.presenter.fragment.BaseViewModelFragment
+import com.example.militaryaccountingapp.presenter.fragment.filter.FilterFragment
+import com.example.militaryaccountingapp.presenter.fragment.filter.FilterViewModel
 import com.example.militaryaccountingapp.presenter.fragment.statistics.StatisticsViewModel.ViewData
+import com.google.android.material.shape.MaterialShapeDrawable
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class StatisticsFragment :
     BaseViewModelFragment<FragmentStatisticsBinding, ViewData, StatisticsViewModel>() {
 
     override val viewModel: StatisticsViewModel by viewModels()
+
+    private val filterViewModel: FilterViewModel by viewModels()
+
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentStatisticsBinding
         get() = FragmentStatisticsBinding::inflate
 
     override fun initializeView() {
+        setupFilterFragment()
+        setupActionBar()
     }
 
     override fun render(data: ViewData) {
         log.d("render")
+    }
+
+
+    private fun renderFilter(data: FilterViewModel.ViewData) {
+        log.d("render")
+    }
+
+    override fun observeData() {
+        super.observeData()
+        viewLifecycleOwner.lifecycleScope.launch {
+            filterViewModel.data
+                .flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
+                .collect { renderFilter(it) }
+        }
+    }
+
+    private fun setupFilterFragment() {
+        childFragmentManager.beginTransaction()
+            .add(R.id.filters, FilterFragment::class.java, null)
+            .commit()
+    }
+
+    private fun setupActionBar() {
+        binding.appBarLayout.statusBarForeground = MaterialShapeDrawable().apply {
+            setTint(resources.getColor(R.color.md_surface, null))
+        }
     }
 }
