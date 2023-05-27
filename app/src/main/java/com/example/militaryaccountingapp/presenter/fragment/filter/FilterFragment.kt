015633a2e5cda036eb00202c96a1acacf2580c53
Erik <Gitarist.Erik@gmail.com>
2023-05-27 09:17:49 +0300

    Update category tree view

--- app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/filter/FilterFragment.kt
+++ app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/filter/FilterFragment.kt
@@ -1,36 +1,24 @@
 package com.example.militaryaccountingapp.presenter.fragment.filter
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.core.util.Pair
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.militaryaccountingapp.R
 import com.example.militaryaccountingapp.databinding.FragmentFiltersBinding
-import com.example.militaryaccountingapp.databinding.ItemTreeNodeViewBinding
 import com.example.militaryaccountingapp.presenter.fragment.BaseViewModelFragment
 import com.example.militaryaccountingapp.presenter.fragment.filter.FilterViewModel.ViewData
-import com.example.militaryaccountingapp.presenter.model.filter.TreeNodeItem
 import com.example.militaryaccountingapp.presenter.shared.adapter.UsersFilterAdapter
 import com.example.militaryaccountingapp.presenter.utils.common.constant.FilterDate
-import com.gg.gapo.treeviewlib.GapoTreeView
-import com.gg.gapo.treeviewlib.model.NodeState
-import com.gg.gapo.treeviewlib.model.NodeViewData
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.divider.MaterialDividerItemDecoration
-import kotlinx.coroutines.launch
 import kotlin.reflect.full.createInstance
 
 
-class FilterFragment : BaseViewModelFragment<FragmentFiltersBinding, ViewData, FilterViewModel>(),
-    GapoTreeView.Listener<TreeNodeItem> {
+class FilterFragment : BaseViewModelFragment<FragmentFiltersBinding, ViewData, FilterViewModel>()   {
 
     override val viewModel: FilterViewModel by activityViewModels()
 
@@ -39,7 +27,6 @@ class FilterFragment : BaseViewModelFragment<FragmentFiltersBinding, ViewData, F
 
     override fun initializeView() {
         setupAdapter()
-        setupTreeView()
         setupDateButtons()
         setupDateModal()
     }
@@ -144,101 +131,5 @@ class FilterFragment : BaseViewModelFragment<FragmentFiltersBinding, ViewData, F
         }
 
         picker.show(childFragmentManager, picker.toString())
-    }
-
-
-    private val treeViewBuilder by lazy {
-        GapoTreeView.Builder.plant<TreeNodeItem>(requireContext())
-            .withRecyclerView(binding.rvItems)
-            .withLayoutRes(R.layout.item_tree_node_view)
-            .setListener(this)
-            .itemMargin(20)
-    }
-
-    private lateinit var treeView: GapoTreeView<TreeNodeItem>
-
-    private fun setupTreeView() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.dataNodes
-                .flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
-                .collect {
-                    try {
-                        binding.rvItems.removeItemDecorationAt(0)
-                    } catch (_: Exception) {
-                    }
-                    treeView = treeViewBuilder
-                        .setData(it)
-                        .build()
-                }
-        }
-    }
-
-
-    override fun onBind(
-        holder: View,
-        position: Int,
-        item: NodeViewData<TreeNodeItem>,
-        bundle: Bundle?
-    ) {
-        with(ItemTreeNodeViewBinding.bind(holder)) {
-
-            ivArrow.visibility = if (item.isLeaf) View.INVISIBLE else View.VISIBLE
-            ivArrow.rotation = if (item.isExpanded) 90f else 0f
-
-            title.text = item.getData().name
-            checkbox.isChecked = item.isSelected
-            checkbox.setOnClickListener {
-                treeView.selectNode(item.nodeId, !item.isSelected) // will trigger onNodeSelected
-            }
-            checkbox.isEnabled = item.nodeState != NodeStateDisabled
-
-//            avatarGroup.dataSource = item.getData().userImagesUrl.toMutableList()
-
-            //toggle node
-            holder.setOnClickListener {
-                if (item.isExpanded) {
-                    treeView.collapseNode(item.nodeId)
-                } else {
-                    treeView.expandNode(item.nodeId)
-                }
-            }
-        }
-    }
-
-    override fun onNodeSelected(
-        node: NodeViewData<TreeNodeItem>,
-        child: List<NodeViewData<TreeNodeItem>>,
-        isSelected: Boolean
-    ) {
-        //set selected for parent node and its child
-        treeView.setSelectedNode(arrayListOf(node).apply { addAll(child) }, isSelected)
-
-        //disable all child
-        treeView.setNodesState(
-            child.map { it.nodeId },
-            if (isSelected) NodeStateDisabled else null
-        )
-
-        // event for viewModel
-        with(node.getData()) {
-            if (!node.isLeaf) {
-                viewModel.changeCategorySelection(
-                    id,
-                    child.map { it.getData().id to it.isLeaf },
-                    isSelected
-                )
-            } else {
-                viewModel.changeItemSelection(id, isSelected)
-            }
-        }
-
-        //update layout
-        treeView.requestUpdateTree()
-    }
-
-    companion object {
-        /** Customize node state **/
-        object NodeStateDisabled : NodeState()
-
     }
 }
---
+++ app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/filter/TreeViewFragment.kt
@@ -0,0 +1,129 @@
+package com.example.militaryaccountingapp.presenter.fragment.filter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.example.militaryaccountingapp.R
+import com.example.militaryaccountingapp.databinding.FragmentTreeViewBinding
+import com.example.militaryaccountingapp.databinding.ItemTreeNodeViewBinding
+import com.example.militaryaccountingapp.presenter.fragment.BaseFragment
+import com.example.militaryaccountingapp.presenter.model.filter.TreeNodeItem
+import com.gg.gapo.treeviewlib.GapoTreeView
+import com.gg.gapo.treeviewlib.model.NodeState
+import com.gg.gapo.treeviewlib.model.NodeViewData
+import kotlinx.coroutines.launch
+
+class TreeViewFragment() : BaseFragment<FragmentTreeViewBinding>(),
+    GapoTreeView.Listener<TreeNodeItem> {
+
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentTreeViewBinding =
+        FragmentTreeViewBinding::inflate
+
+    private val viewModel: FilterViewModel by activityViewModels()
+
+
+    override fun initializeView() {
+        setupTreeView()
+    }
+
+
+    private val treeViewBuilder by lazy {
+        GapoTreeView.Builder.plant<TreeNodeItem>(requireContext())
+            .withRecyclerView(binding.rvItems)
+            .withLayoutRes(R.layout.item_tree_node_view)
+            .setListener(this)
+            .itemMargin(20)
+    }
+
+    private lateinit var treeView: GapoTreeView<TreeNodeItem>
+
+    private fun setupTreeView() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.dataNodes
+                .flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
+                .collect {
+                    try {
+                        binding.rvItems.removeItemDecorationAt(0)
+                    } catch (_: Exception) {
+                    }
+                    treeView = treeViewBuilder
+                        .setData(it)
+                        .build()
+                }
+        }
+    }
+
+    override fun onBind(
+        holder: View,
+        position: Int,
+        item: NodeViewData<TreeNodeItem>,
+        bundle: Bundle?
+    ) {
+        with(ItemTreeNodeViewBinding.bind(holder)) {
+
+            ivArrow.visibility = if (item.isLeaf) View.INVISIBLE else View.VISIBLE
+            ivArrow.rotation = if (item.isExpanded) 90f else 0f
+
+            title.text = item.getData().name
+            checkbox.isChecked = item.isSelected
+            checkbox.setOnClickListener {
+                treeView.selectNode(item.nodeId, !item.isSelected) // will trigger onNodeSelected
+            }
+            checkbox.isEnabled = item.nodeState != NodeStateDisabled
+
+//            avatarGroup.dataSource = item.getData().userImagesUrl.toMutableList()
+
+            //toggle node
+            holder.setOnClickListener {
+                if (item.isExpanded) {
+                    treeView.collapseNode(item.nodeId)
+                } else {
+                    treeView.expandNode(item.nodeId)
+                }
+            }
+        }
+    }
+
+    override fun onNodeSelected(
+        node: NodeViewData<TreeNodeItem>,
+        child: List<NodeViewData<TreeNodeItem>>,
+        isSelected: Boolean
+    ) {
+        //set selected for parent node and its child
+        treeView.setSelectedNode(arrayListOf(node).apply { addAll(child) }, isSelected)
+
+        //disable all child
+        treeView.setNodesState(
+            child.map { it.nodeId },
+            if (isSelected) NodeStateDisabled else null
+        )
+
+        // event for viewModel
+        with(node.getData()) {
+            if (!node.isLeaf) {
+                viewModel.changeCategorySelection(
+                    id,
+                    child.map { it.getData().id to it.isLeaf },
+                    isSelected
+                )
+            } else {
+                viewModel.changeItemSelection(id, isSelected)
+            }
+        }
+
+        //update layout
+        treeView.requestUpdateTree()
+    }
+
+
+    companion object {
+        /** Customize node state **/
+        object NodeStateDisabled : NodeState()
+
+    }
+}
