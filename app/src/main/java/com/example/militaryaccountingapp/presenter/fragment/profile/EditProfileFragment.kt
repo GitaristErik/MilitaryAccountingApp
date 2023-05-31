1d0b2277851df5ab29c6ac06ac89703039822f2b
Erik <Gitarist.Erik@gmail.com>
2023-05-31 19:45:40 +0300

    Write Add phone button layout

--- app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/profile/EditProfileFragment.kt
+++ app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/profile/EditProfileFragment.kt
@@ -1,12 +1,17 @@
 package com.example.militaryaccountingapp.presenter.fragment.profile
 
 import android.view.LayoutInflater
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.LinearLayout.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.updateMargins
 import androidx.fragment.app.viewModels
+import com.example.militaryaccountingapp.R
 import com.example.militaryaccountingapp.databinding.FragmentEditProfileBinding
 import com.example.militaryaccountingapp.presenter.fragment.BaseViewModelFragment
 import com.example.militaryaccountingapp.presenter.fragment.profile.ProfileViewModel.ViewData
+import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,6 +24,7 @@ class EditProfileFragment :
 
     override fun initializeView() {
         setupActionBar()
+        setupPhone()
     }
 
     private fun setupActionBar() {
@@ -31,6 +37,67 @@ class EditProfileFragment :
             }
         }
     }
+
+    private fun setupPhone() {
+        binding.addPhoneLayout.setEndIconOnClickListener {
+            if(binding.addPhone.text.isNullOrEmpty()) return@setEndIconOnClickListener
+            makePhoneEditText(binding.addPhone.text.toString()) {
+                binding.phoneContainer.removeView(it)
+            }.also {
+                binding.phoneContainer.addView(it)
+                binding.addPhone.setText("")
+            }
+        }
+    }
+
+    private fun makePhoneEditText(
+        phone: String,
+        onRemoveListener: OnClickListener
+    ): TextInputEditText =
+        TextInputEditText(requireContext()).apply {
+            val id = generateId()
+            setId(id)
+            layoutParams = LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT
+            ).apply {
+                val margin = resources.getDimension(R.dimen.margin_small_extra).toInt()
+                updateMargins(top = margin, bottom = margin)
+            }
+            isElegantTextHeight = true
+            hint = getString(R.string.profile_phone_hint)
+            inputType = android.text.InputType.TYPE_CLASS_PHONE
+            setText(phone)
+
+            // set drawable and listener
+            setCompoundDrawablesRelativeWithIntrinsicBounds(
+                0, 0, R.drawable.ic_remove_24dp, 0
+            )
+            compoundDrawablePadding = resources.getDimension(R.dimen.padding_small).toInt()
+            setOnTouchListener { _, event ->
+                if (event.action == android.view.MotionEvent.ACTION_UP) {
+                    if (event.rawX >= (right - compoundDrawables[2].bounds.width())) {
+                        onRemoveListener.onClick(this)
+                        return@setOnTouchListener true
+                    } else {
+                        view?.performClick()
+                    }
+                }
+                return@setOnTouchListener false
+            }
+        }
+
+    private fun generateId(): Int {
+        // Get the current time in milliseconds.
+        val time = System.currentTimeMillis()
+
+        // Get the current thread ID.
+        val threadId = Thread.currentThread().id
+
+        // Return a unique ID that is based on the current time and thread ID.
+        return (time * 10000 + threadId).toInt()
+    }
+
 
     override fun render(data: ViewData) {
         log.d("render")
