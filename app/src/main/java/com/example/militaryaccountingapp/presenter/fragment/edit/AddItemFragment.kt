c7fc7ad5a115f7a31ed1eec1e592f086a6c59e16
Erik <Gitarist.Erik@gmail.com>
2023-06-01 20:11:45 +0300

    Write Add item fragment xml and change login button in profile edit

--- app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/edit/AddItemFragment.kt
+++ app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/edit/AddItemFragment.kt
@@ -2,15 +2,19 @@ package com.example.militaryaccountingapp.presenter.fragment.edit
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.navGraphViewModels
 import com.example.militaryaccountingapp.R
 import com.example.militaryaccountingapp.databinding.FragmentAddItemBinding
 import com.example.militaryaccountingapp.presenter.fragment.BaseViewModelFragment
 import com.example.militaryaccountingapp.presenter.fragment.edit.AddOrEditViewModel.ViewData
+import com.mcdev.quantitizerlibrary.AnimationStyle
+import com.mcdev.quantitizerlibrary.QuantitizerListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AddItemFragment : BaseViewModelFragment<FragmentAddItemBinding, ViewData, AddOrEditViewModel>() {
+class AddItemFragment :
+    BaseViewModelFragment<FragmentAddItemBinding, ViewData, AddOrEditViewModel>() {
 
     override val viewModel: AddOrEditViewModel by navGraphViewModels(R.id.mobile_navigation)
 
@@ -18,9 +22,9 @@ class AddItemFragment : BaseViewModelFragment<FragmentAddItemBinding, ViewData, 
         get() = FragmentAddItemBinding::inflate
 
 
-        override fun initializeView() {
+    override fun initializeView() {
+        setupQuantity()
         setupQrCodeScanner()
-
     }
 
     private fun setupQrCodeScanner() {
@@ -28,6 +32,26 @@ class AddItemFragment : BaseViewModelFragment<FragmentAddItemBinding, ViewData, 
 
         // Add the barcode scanner fragment to the view hierarchy.
 //        BarcodeView
+    }
+
+    private fun setupQuantity() {
+        binding.layoutQuantity.apply {
+            setQuantitizerListener(object : QuantitizerListener {
+                override fun onIncrease() {
+                    Toast.makeText(context, "inc", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onDecrease() {
+                    Toast.makeText(context, "dec", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onValueChanged(value: Int) {
+                    Toast.makeText(context, "value changed to : $value", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+            textAnimationStyle = AnimationStyle.FALL_IN
+        }
     }
 
     override fun render(data: ViewData) {
