9baa7cc663bd8911bf86c99a90356e7730f5113d
Erik <Gitarist.Erik@gmail.com>
2023-06-01 20:33:07 +0300

    simple xml for AddCategoryFragment

--- app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/edit/AddCategoryFragment.kt
+++ app/src/main/java/com/example/militaryaccountingapp/presenter/fragment/edit/AddCategoryFragment.kt
@@ -4,18 +4,18 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.navGraphViewModels
 import com.example.militaryaccountingapp.R
-import com.example.militaryaccountingapp.databinding.FragmentAddItemBinding
+import com.example.militaryaccountingapp.databinding.FragmentAddCategoryBinding
 import com.example.militaryaccountingapp.presenter.fragment.BaseViewModelFragment
 import com.example.militaryaccountingapp.presenter.fragment.edit.AddOrEditViewModel.ViewData
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AddCategoryFragment : BaseViewModelFragment<FragmentAddItemBinding, ViewData, AddOrEditViewModel>() {
+class AddCategoryFragment : BaseViewModelFragment<FragmentAddCategoryBinding, ViewData, AddOrEditViewModel>() {
 
     override val viewModel: AddOrEditViewModel by navGraphViewModels(R.id.mobile_navigation)
 
-    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentAddItemBinding
-        get() = FragmentAddItemBinding::inflate
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentAddCategoryBinding
+        get() = FragmentAddCategoryBinding::inflate
 
 
     override fun initializeView()  {
